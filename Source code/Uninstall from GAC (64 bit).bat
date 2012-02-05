8637ffc514f8f57a0807feee62a75d23efdd23db
afsherman_cp <SND\afsherman_cp>
2012-02-05 15:55:32 -0800

    - Changed deploying system

--- Source code/Uninstall from GAC (64 bit).bat
+++ Source code/Uninstall from GAC (64 bit).bat
@@ -3,8 +3,8 @@ set PATH=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools
 chdir /d %~dp0
 cd ..\
 @echo on
-gacutil /u "%CD%\Tools and Resources\Assembly dependencies\Microsoft.Expression.Drawing.dll"
-gacutil /u "%CD%\Tools and Resources\Assembly dependencies\Microsoft.Expression.Interactions.dll"
-gacutil /u "%CD%\Tools and Resources\Assembly dependencies\Microsoft.Windows.Shell.dll"
-gacutil /u "%CD%\Tools and Resources\Assembly dependencies\System.Windows.Interactivity.dll"
+gacutil /u "Microsoft.Expression.Drawing"
+gacutil /u "Microsoft.Expression.Interactions"
+gacutil /u "Microsoft.Windows.Shell"
+gacutil /u "System.Windows.Interactivity"
 pause
