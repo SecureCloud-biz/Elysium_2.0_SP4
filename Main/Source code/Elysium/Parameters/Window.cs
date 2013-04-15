d902df5439840afb90162f06f6757c851fc76ce8
afsherman_cp <SND\afsherman_cp>
2013-04-15 16:51:36 -0700

    - Added tooltip support for Minimize, Maximize, Restore and Close buttons

--- Main/Source code/Elysium/Parameters/Window.cs
+++ Main/Source code/Elysium/Parameters/Window.cs
@@ -13,8 +13,7 @@ namespace Elysium.Parameters
         [PublicAPI]
         public static readonly DependencyProperty ResizeBorderThicknessProperty =
             DependencyProperty.RegisterAttached("ResizeBorderThickness", typeof(Thickness), typeof(Window),
-                                                new FrameworkPropertyMetadata(new Thickness(3d), FrameworkPropertyMetadataOptions.AffectsArrange,
-                                                                              null, ThicknessUtil.CoerceNonNegative));
+                                                new FrameworkPropertyMetadata(new Thickness(3d), FrameworkPropertyMetadataOptions.AffectsArrange, null, ThicknessUtil.CoerceNonNegative));
 
         [PublicAPI]
         [AttachedPropertyBrowsableForType(typeof(Controls.Window))]
@@ -31,6 +30,78 @@ namespace Elysium.Parameters
         {
             ValidationHelper.NotNull(obj, "obj");
             obj.SetValue(ResizeBorderThicknessProperty, value);
+        }
+
+        [PublicAPI]
+        public static readonly DependencyProperty MinimizeButtonToolTipProperty = DependencyProperty.RegisterAttached("MinimizeButtonToolTip", typeof(object), typeof(Window),
+                                                                                                                      new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.None));
+
+        [PublicAPI]
+        [AttachedPropertyBrowsableForType(typeof(Controls.Window))]
+        public static void SetMinimizeButtonToolTip([NotNull] Controls.Window obj, object value)
+        {
+            ValidationHelper.NotNull(obj, "obj");
+            obj.SetValue(MinimizeButtonToolTipProperty, value);
+        }
+
+        [PublicAPI]
+        public static object GetMinimizeButtonToolTip([NotNull] Controls.Window obj)
+        {
+            return obj.GetValue(MinimizeButtonToolTipProperty);
+        }
+
+        [PublicAPI]
+        public static readonly DependencyProperty MaximizeButtonToolTipProperty = DependencyProperty.RegisterAttached("MaximizeButtonToolTip", typeof(object), typeof(Window),
+                                                                                                                      new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.None));
+
+        [PublicAPI]
+        [AttachedPropertyBrowsableForType(typeof(Controls.Window))]
+        public static void SetMaximizeButtonToolTip([NotNull] Controls.Window obj, object value)
+        {
+            ValidationHelper.NotNull(obj, "obj");
+            obj.SetValue(MaximizeButtonToolTipProperty, value);
+        }
+
+        [PublicAPI]
+        public static object GetMaximizeButtonToolTip([NotNull] Controls.Window obj)
+        {
+            return obj.GetValue(MaximizeButtonToolTipProperty);
+        }
+
+        [PublicAPI]
+        public static readonly DependencyProperty RestoreButtonToolTipProperty = DependencyProperty.RegisterAttached("RestoreButtonToolTip", typeof(object), typeof(Window),
+                                                                                                                      new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.None));
+
+        [PublicAPI]
+        [AttachedPropertyBrowsableForType(typeof(Controls.Window))]
+        public static void SetRestoreButtonToolTip([NotNull] Controls.Window obj, object value)
+        {
+            ValidationHelper.NotNull(obj, "obj");
+            obj.SetValue(RestoreButtonToolTipProperty, value);
+        }
+
+        [PublicAPI]
+        public static object GetRestoreButtonToolTip([NotNull] Controls.Window obj)
+        {
+            return obj.GetValue(RestoreButtonToolTipProperty);
+        }
+
+        [PublicAPI]
+        public static readonly DependencyProperty CloseButtonToolTipProperty = DependencyProperty.RegisterAttached("CloseButtonToolTip", typeof(object), typeof(Window),
+                                                                                                                      new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.None));
+
+        [PublicAPI]
+        [AttachedPropertyBrowsableForType(typeof(Controls.Window))]
+        public static void SetCloseButtonToolTip([NotNull] Controls.Window obj, object value)
+        {
+            ValidationHelper.NotNull(obj, "obj");
+            obj.SetValue(CloseButtonToolTipProperty, value);
+        }
+
+        [PublicAPI]
+        public static object GetCloseButtonToolTip([NotNull] Controls.Window obj)
+        {
+            return obj.GetValue(CloseButtonToolTipProperty);
         }
     }
 }
