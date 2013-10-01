c82fc93fe1a533ec26e9bf828e4c260bf154b951
afsherman_cp <SND\afsherman_cp>
2013-10-01 09:57:24 -0700

    Elysium 2.0: - Signed PowerShell scripts Elysium 2.1: - Changeset is still broken

--- 2.0/Source code/Build.ps1
+++ 2.0/Source code/Build.ps1
@@ -47,3 +47,38 @@ Build-Documentation -Version $Version -Framework $Framework -Language ru -Thread
 # Build installation projects
 Build-Installation -Version $Version -Framework $Framework -Configuration Debug -Transform $True -AssemblyKey $AssemblyKey -SignatureKey $SignatureKey -SignatureParams $SignatureParams -Threads $Threads -Checkout $Checkout
 Build-Installation -Version $Version -Framework $Framework -Configuration Release -Transform $True -AssemblyKey $AssemblyKey -SignatureKey $SignatureKey -SignatureParams $SignatureParams -Threads $Threads -Checkout $Checkout
+# SIG # Begin signature block
+# MIIGAwYJKoZIhvcNAQcCoIIF9DCCBfACAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUrQYSMLz2YXDPO5Nzp0oOxbVb
+# x06gggN8MIIDeDCCAmCgAwIBAgIQOmW6/axc969KEPAZJomtbzANBgkqhkiG9w0B
+# AQ0FADA8MTowOAYDVQQDEzFBbGVrc2FuZHIgVmlzaG55YWtvdiAtIFRlbXBvcmFy
+# eSByb290IGNlcnRpZmljYXRlMB4XDTEyMDkxMDA3MjgzMVoXDTM5MTIzMTIzNTk1
+# OVowKjEoMCYGA1UEAxMfRWx5c2l1bSAtIFRlbXBvcmFyeSBjZXJ0aWZpY2F0ZTCC
+# ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANKG/OMKgZrEcVvIgRSMjSij
+# sfG9wTCwi2RFlphVJtuWfJ09caUcQw8xIr95yyMdyyOeFNLcovmqLLX9GFt7/qKc
+# Jh2m+EVvU4RvPs+YjfQio/T8u+ZI8dndgwegKcs3B0VLIiX35JHe5SSyWUwnAqjH
+# NdW0fBO18k0xjRGN30TCdqSRUjdNu6zOvOF6GEsBvnW677vY2mIic3wFq1Q5//WQ
+# GmCjOkv75spzsI95MNOaj0CTj5PE0bfm4OIz55U6jT+2SOxFxDVp0y+lAAn7L1i2
+# ymeyfBzM3w53gIQrOKl2CT/fVldeNQy1/cYWWBgHo+3EZKk7TnfkE8++YPrBUb0C
+# AwEAAaOBhzCBhDATBgNVHSUEDDAKBggrBgEFBQcDAzBtBgNVHQEEZjBkgBBnVlWU
+# Fuyg6Mb0/essbehVoT4wPDE6MDgGA1UEAxMxQWxla3NhbmRyIFZpc2hueWFrb3Yg
+# LSBUZW1wb3Jhcnkgcm9vdCBjZXJ0aWZpY2F0ZYIQPtMhNrOL6YVORyl7mbhbxDAN
+# BgkqhkiG9w0BAQ0FAAOCAQEAKYr6EQqKHNMP8kYTTdEjG8S0ixiTEcVE01lzA0Cu
+# XS1I5GkpAU66qlu/u+Vc72hwxV8XkTICM2bYdqhz/7PmH8hJaYJfLww2rOoqsATi
+# 9UVznlmT0b6BLojUBLQnTD2TKPHg9xIsfbFVyKYMuXmOSA+IXaaExWQOMcVAw7pc
+# paL3ZjrjbwLHQlo8bhS01KgL78Mu4ShDhCf2Y/U80kooUWYE5SLmx5XS38ZE+Bgw
+# HCp04tQrvW9uYGJ+viDVpq1JPXJ/xTtsSR5xz9kPVC5iI6Hlo62NG3MND/P4yXFe
+# MkQGW1uxIX+i7kegMemoGksP+otBcwhFteyFbAn0QawuvjGCAfEwggHtAgEBMFAw
+# PDE6MDgGA1UEAxMxQWxla3NhbmRyIFZpc2hueWFrb3YgLSBUZW1wb3Jhcnkgcm9v
+# dCBjZXJ0aWZpY2F0ZQIQOmW6/axc969KEPAZJomtbzAJBgUrDgMCGgUAoHgwGAYK
+# KwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIB
+# BDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQU
+# BN6ozqh697Z1Km5hiqeGwCRcBiowDQYJKoZIhvcNAQEBBQAEggEAl14My7N+dGfe
+# l7PvHt/FeZ0tMadHFSzBQBzEKwqWp3hKUkkV3RYQ/Jpv/Ghrlyt77g8hiFzBIMJh
+# XZUKy6TwR5Ko+D377xlMahngzoRSxtPPLU6/wRJdGX7e1zr9oQFVFx8gCFE7vxFo
+# RyDd/i15hp9/kwCRbJTQgd7kMU7bMeaaVYoLSZXa0QdsyLGV7FoO13vfb1Uh+2sI
+# iMsaON2skVfcRXNPVp/uZOWMqjOXt+WueQloQqGiPZBw+gPQKo6MA9qldIiP6ptF
+# fv9wCSpMi3p1eYJ8VMNFzSnbFdHt70nOW5H5/SyfgOAUNvVo25Shrxg9PPhrJ5vs
+# rnF5IuPpkg==
+# SIG # End signature block
